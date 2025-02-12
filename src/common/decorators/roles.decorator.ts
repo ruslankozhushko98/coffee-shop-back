a9d9b4b0600f72b8c12cd1b48c70b6f5dc2ed87b
ruslankozhushko98 <ruslankozhushko98@gmail.com>
2025-02-12 16:19:59 -0600

    feat: update roles decorator params types;

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { ROLES } from '@prisma/client';
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const Roles = (...roles: Array<ROLES>) => SetMetadata('roles', roles);
